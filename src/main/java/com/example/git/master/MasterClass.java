6a15e405da008588e5b680f379a678ea7f026c4f
pavan-code <pavankumaranguluri1@gmail.com>
2021-10-25 12:00:46 +0530

    added new package

---
+++ src/main/java/com/example/git/master/MasterClass.java
@@ -0,0 +1,10 @@
+package com.example.git.master;
+
+public class MasterClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("in master class");
+	}	
+
+}
